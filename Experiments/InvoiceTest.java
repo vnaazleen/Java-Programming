5d7d1b992af67a929847b77676a21fd481c1bda5
Vaseem Naazleen Shaik <54474853+vnaazleen@users.noreply.github.com>
2021-02-09 19:08:09 +0530

    Experiment - 1 Invoice

--- Experiments/InvoiceTest.java
+++ Experiments/InvoiceTest.java
@@ -59,7 +59,7 @@ public class InvoiceTest {
                    myInvoice = new Invoice("19BQ1A05L1", "My First Invoice :)", 5, 2.0);
                    myInvoice.setPrice(5.0);
                    System.out.println("Number: " + myInvoice.getNumber());
-                   System.out.println("Descriptio: " + myInvoice.getDescription());
+                   System.out.println("Description: " + myInvoice.getDescription());
                    System.out.println("Quantity: " + myInvoice.getQunatity());
                    System.out.println("Price:" + myInvoice.getPrice());
                    System.out.println("invoiceAmount: " + myInvoice.getInvoiceAmount());
