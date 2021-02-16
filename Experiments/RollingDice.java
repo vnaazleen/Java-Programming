3965172c1e12d9d5fd1fcac92f2c4c31c09126e5
Vaseem Naazleen Shaik <54474853+vnaazleen@users.noreply.github.com>
2021-02-16 19:16:48 +0530

    Experiment - 7 (Rolling dice)

--- Experiments/RollingDice.java
+++ Experiments/RollingDice.java
@@ -3,10 +3,10 @@ public class RollingDice {
 		int rolls = 10000;
 		int count = 0;
 		for (int i=0; i < rolls; i++) {
-			int dice1 = (int) (Math.random() * 10) % 6;
-			int dice2 = (int) (Math.random() * 10) % 6;
+			int dice1 = 1 + (int) (Math.random() * 10) % 6;
+			int dice2 = 1 + (int) (Math.random() * 10) % 6;
 			
-			if (dice1 == dice2*2 || dice1*2==dice2) {
+			if (dice1 == dice2){
 				count += 1;
 			}
 		}
