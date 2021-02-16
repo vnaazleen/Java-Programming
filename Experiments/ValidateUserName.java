781c9e3a7fdcc1be42f79909c5e47fad5c40beb0
Vaseem Naazleen Shaik <54474853+vnaazleen@users.noreply.github.com>
2021-02-16 19:18:30 +0530

    Experiment-8 ValidateUserName using Regex

--- Experiments/ValidateUserName.java
+++ Experiments/ValidateUserName.java
@@ -1,18 +1,18 @@
-package Experiments;
-
 import java.util.Scanner;
-
-// Expression - 8
-
 public class ValidateUserName {
 	public static void main(String args[]) {
 		Scanner in = new Scanner(System.in);
 		
-		String UserName = in.next();
+		String UserName = in.nextLine();
 		
 		// UserName can have Letters, Numbers, Underscore characters and it shd be more than 8 characters long & less than 16 characters long
 		String regex = "\\w+";
 		// \w matches any word character (equal to [a-zA-Z0-9_])
+		
+		if (UserName.contains(" ")) {
+			System.out.println("Invalid username. Username shouldn't contain spaces");
+			return;
+		}
 		
 		if (UserName.length() >= 8 && UserName.length() < 16) {
 			if (UserName.matches(regex)) {
